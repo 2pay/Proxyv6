164d02a4e81ab4b73a87b2e538f457b632f9b485
2pay <cnttlc@gmail.com>
2022-12-21 10:04:46 +0700

    Update script install

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ install_3proxy() {
     echo "installing 3proxy"
     URL="https://raw.githubusercontent.com/2pay/Proxyv6/main/3proxy-0.9.3.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy-0.9.3
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
